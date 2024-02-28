834e1acdf2fb2de9d5aa49adb2f3b4e1ad416693
NoorulNisa <gsnoor2k@gmail.com>
2024-02-28 06:17:09 +0500

    added templete and required settings

--- CodingNinjas/CodingNinjas/urls.py
+++ CodingNinjas/CodingNinjas/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myPortfolio.urls'))
 
-]
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
